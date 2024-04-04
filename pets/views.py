a7c2bbefdeccae6e2316dc48c840eb83fe277e48
rabhi alqadi <rabhialqadi22@gmail.com>
2024-04-03 18:41:00 -0700

    models and views set

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import TaskSerializer, TagSerializer  # Import TaskSerializer and TagSerializer
+from .models import Task, Tag  # Import Task and Tag models
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.AllowAny]  # Adjust permissions as needed
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.AllowAny]  # Adjust permissions as needed
