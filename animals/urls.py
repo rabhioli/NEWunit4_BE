5eb23e83ae9008d0243c5c3e10cd8a4c56feba91
rabhi alqadi <rabhialqadi22@gmail.com>
2024-04-04 19:55:58 -0700

    fixed -r

--- animals/urls.py
+++ animals/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from rest_framework import routers
 from pets.views import TagViewSet, TaskViewSet
 
-
-router=routers.DefaultRouter()
-router.register(r'pets', TagViewSet, TaskViewSet)
+router = routers.DefaultRouter()
+router.register(r'tags', TagViewSet)
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
